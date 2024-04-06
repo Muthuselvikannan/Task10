6e963d0245c166442d43e9766f962706d2134965
Muthuselvikannan <muthuselvitamizh@gmail.com>
2024-04-06 14:03:15 +0530

    add

---
+++ script.js
@@ -0,0 +1,81 @@
+let count = 11;
+setTimeout(() => {
+  count--;
+  console.log(count);
+  var screen = document.querySelector("#countdown");
+  screen.innerText = count;
+
+  setTimeout(() => {
+    count--;
+    console.log(count);
+    var screen = document.querySelector("#countdown");
+    screen.innerText = count;
+
+    setTimeout(() => {
+      count--;
+      console.log(count);
+      var screen = document.querySelector("#countdown");
+      screen.innerText = count;
+
+      setTimeout(() => {
+        count--;
+        console.log(count);
+        var screen = document.querySelector("#countdown");
+        screen.innerText = count;
+
+        setTimeout(() => {
+          count--;
+          console.log(count);
+          var screen = document.querySelector("#countdown");
+          screen.innerText = count;
+
+          setTimeout(() => {
+            count--;
+            console.log(count);
+            var screen = document.querySelector("#countdown");
+            screen.innerText = count;
+
+            setTimeout(() => {
+              count--;
+              console.log(count);
+              var screen = document.querySelector("#countdown");
+              screen.innerText = count;
+
+              setTimeout(() => {
+                count--;
+                console.log(count);
+                var screen = document.querySelector("#countdown");
+                screen.innerText = count;
+
+                setTimeout(() => {
+                  count--;
+                  console.log(count);
+                  var screen = document.querySelector("#countdown");
+                  screen.innerText = count;
+
+                  setTimeout(() => {
+                    count--;
+                    console.log(count);
+                    var screen = document.querySelector("#countdown");
+                    screen.innerText = count;
+
+                    setTimeout(() => {
+                      count--;
+                      console.log(count);
+                      var screen = document.querySelector("#countdown");
+                      screen.innerText = count;
+                      clearTimeout();
+                      console.log("Happy Independence Day");
+                      var screen = document.querySelector("#countdown");
+                      screen.innerText = "Happy Independence Day!!!";                    
+                    }, 2000);
+                  }, 2000);
+                }, 2000);
+              }, 2000);
+            }, 2000);
+          }, 2000);
+        }, 2000);
+      }, 2000);
+    }, 2000);
+  }, 2000);
+}, 2000);
